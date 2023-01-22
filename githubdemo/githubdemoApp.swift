002199dbd70589c6a5a3bc41a92d73ace308aef6
Balasoiu Nicolae <nicolaebalasoiu97@gmail.com>
2023-01-23 00:10:05 +0200

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by Nicolae Balasoiu on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
